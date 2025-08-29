fad63287f419ce1f7733f25987f0657fc27a1e8a
Sanju Gotur <sanjugotur8@gmail.com>
2025-08-29 20:10:10 +0530

    port.js

---
+++ port.js
@@ -0,0 +1,18 @@
+// Navbar toggle
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// Smooth scrolling
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+    navLinks.classList.remove("active");
+  });
+});
